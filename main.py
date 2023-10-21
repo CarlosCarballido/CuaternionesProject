e518781f622af5de9e5da882f930463a05a24865
carlooscarbauvigo <carlos.carballido@alumnos.uvigo.es>
2023-10-21 02:58:30 +0200

    Add Anotations and type Anottations

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+# This module is used to do mathematical operations (pass to dregrees, tan, sen, cos, etc).
 import math
 
-
+# This class is used to create a quaternion object.
 class Quaternions:
-    def __init__(self, w, x, y, z):
+    def __init__(self, w:float, x:float, y:float, z:float):
         self.__w = w
         self.__x = x
         self.__y = y
@@ -44,7 +45,8 @@ class Quaternions:
     def __str__(self) -> str:
         return "\tQuaternion coefficients:\nW: " + str(self.w) + "\nX: " + str(self.x) + "\nY: " + str(self.y) + "\nZ: " + str(self.z)
 
-    def to_euler_angles(self):
+    # This method is used to convert a quaternion to euler angles.
+    def to_euler_angles(self) -> tuple[float, float, float]:
         t0 = +2.0 * (self.w * self.x + self.y * self.z)
         t1 = +1.0 - 2.0 * (self.x * self.x + self.y * self.y)
         X = math.degrees(math.atan2(t0, t1))
@@ -59,3 +61,10 @@ class Quaternions:
         Z = math.degrees(math.atan2(t3, t4))
 
         return X, Y, Z
+
+
+q = Quaternions(0.5, 0.33, 0.77, 0.67)
+X, Y, Z = q.to_euler_angles()
+print("Roll:", X)
+print("Roll:", Y)
+print("Roll:", Z)
