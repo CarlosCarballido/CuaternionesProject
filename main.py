e617453417d63ee5fce09bce2201e39a328ecfc6
carlooscarbauvigo <carlos.carballido@alumnos.uvigo.es>
2023-10-21 02:19:07 +0200

    Add Class Quaternions with constructor, getters and setters

--- main.py
+++ main.py
@@ -1,18 +1,48 @@
 import math
 
-def Quaternions(w:float, x:float, y:float, z:float):
 
-    t0 = +2.0 * (w * x + y * z)
-    t1 = +1.0 - 2.0 * (x * x + y * y)
-    X = math.degrees(math.atan2(t0, t1))
+class Quaternions:
+    def __init__(self, w, x, y, z):
+        self.__w = w
+        self.__x = x
+        self.__y = y
+        self.__z = z
 
-    t2 = +2.0 * (w * y - z * x)
-    t2 = +1.0 if t2 > +1.0 else t2
-    t2 = -1.0 if t2 < -1.0 else t2
-    Y = math.degrees(math.asin(t2))
+    @property
+    def w(self):
+        return self.__w
 
-    t3 = +2.0 * (w * z + x * y)
-    t4 = +1.0 - 2.0 * (y * y + z * z)
-    Z = math.degrees(math.atan2(t3, t4))
+    @w.setter
+    def w(self, value):
+        self.__w = value
 
-    return X, Y, Z
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        self.__x = value
+
+    @property
+    def y(self):
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        self.__y = value
+
+    @property
+    def z(self):
+        return self.__z
+
+    @z.setter
+    def z(self, value):
+        self.__z = value
+
+    @property
+    def __str__(self) -> str:
+        print("\tQuaternion coefficients:\nW: " + self.w +
+              "\nX: " + self.x + "\nY: " + self.y + "\nZ: " + self.z)
+
+
